c75f92e373814f57153ab90682a92232a8446238
nasangw <good617boy@gmail.com>
2019-12-13 09:45:43 +0900

    Add, Edit - 배우는 단계!

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './Color';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello!</Text>
+      <Text style={styles.description}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -14,8 +15,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    backgroundColor: '#000',
     padding: '5%',
   },
+  title: {
+    color: 'purple',
+    fontSize: 40,
+  },
+  description: {
+    // color: '#fff',
+    color: colors.white,
+  }
 });
